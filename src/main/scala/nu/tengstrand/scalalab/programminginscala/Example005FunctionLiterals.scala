ada409c0b0dc9606b8ff5f367a5c04deff32cce7
Joakim Tengstrand <joakim.tengstrand@.jaybis.local>
2011-11-22 21:37:13 +0100

    Added comments.

--- src/main/scala/nu/tengstrand/scalalab/programminginscala/Example005FunctionLiterals.scala
+++ src/main/scala/nu/tengstrand/scalalab/programminginscala/Example005FunctionLiterals.scala
@@ -1,7 +1,7 @@
 package nu.tengstrand.scalalab.programminginscala
 
 /**
- * Example of 'function literal'
+ * Example of function literals.
  * Page 148 in the book "Programming in Scala, 2nd edition".
  */
 object Example005FunctionLiterals {
@@ -10,7 +10,7 @@ object Example005FunctionLiterals {
     val integers = List(1,2,3,4,5)
     val odds = integers.filter(_ % 2 == 1)
     val evenFilter = (_:Int) % 2 == 0
-    println(odds)
-    println(integers.filter(evenFilter))
+    println(odds) // output: List(1, 3, 5)
+    println(integers.filter(evenFilter))  // output: List(2, 4)
   }
 }
